c67b9d45757365b794e45be8c3dce899dd221ac0
Emran Hossen <dev.emranhossen@gmail.com>
2026-06-14 14:33:38 +0600

    project complete

--- src/app/test-icons/page.tsx
+++
@@ -1,9 +0,0 @@
-import TestIcons from '@/components/test-icons';
-
-export default function TestIconsPage() {
-  return (
-    <div className="min-h-screen">
-      <TestIcons />
-    </div>
-  );
-}
